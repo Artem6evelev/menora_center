0c64b947a6f34239fc3df5b8bf74b1732ae5a4f3
Артем Шевелев <artem6evelev92@gmail.com>
2026-03-01 23:00:42 +0200

    fix: blog path for i18n

--- components/subheading.tsx
+++ components/subheading.tsx
@@ -1,27 +1,29 @@
 import { cn } from "@/lib/utils";
-import { AnimationProps, MotionProps } from "framer-motion";
+import type { MotionProps } from "framer-motion";
 import React from "react";
 import Balancer from "react-wrap-balancer";
+
+type SubheadingProps = {
+  className?: string;
+  as?: React.ElementType;
+  children: React.ReactNode;
+} & React.HTMLAttributes<HTMLHeadingElement> &
+  MotionProps;
 
 export const Subheading = ({
   className,
   as: Tag = "h2",
   children,
   ...props
-}: {
-  className?: string;
-  as?: any;
-  children: any;
-  props?: React.HTMLAttributes<HTMLHeadingElement | AnimationProps>;
-} & MotionProps &
-  React.HTMLAttributes<HTMLHeadingElement | AnimationProps>) => {
+}: SubheadingProps) => {
   return (
     <Tag
       className={cn(
-        "text-sm md:text-base  max-w-4xl text-left my-4 mx-auto",
+        "text-sm md:text-base max-w-4xl text-left my-4 mx-auto",
         "text-muted text-center font-normal dark:text-muted-dark",
-        className
+        className,
       )}
+      {...props}
     >
       <Balancer>{children}</Balancer>
     </Tag>
