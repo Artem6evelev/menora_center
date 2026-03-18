6d674d5740d864a4106033f8b547b020a9ba9e1f
Артем Шевелев <artem6evelev92@gmail.com>
2026-03-18 21:29:51 +0200

    work

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -9,14 +9,26 @@ export const Logo = () => {
       href="/"
       className="relative z-20 flex items-center justify-center outline-none transition-transform active:scale-95 hover:opacity-90"
     >
+      {/* Логотип для СВЕТЛОЙ темы (скрывается в темной) */}
       <Image
         src="/logos/menora-logo.svg"
         alt="Menora Center"
-        width={180} // Увеличили базовую ширину
-        height={60} // Увеличили базовую высоту
+        width={180}
+        height={60}
         priority
-        // Увеличили высоту с h-8/10 до h-10/14. object-contain не даст ему сплющиться.
-        className="h-13 md:h-14 w-auto object-contain dark:drop-shadow-md"
+        // h-10 на мобилках, h-12 на планшетах, h-14 на десктопе
+        className="h-10 md:h-12 lg:h-14 w-auto object-contain block dark:hidden"
+      />
+
+      {/* Логотип для ТЕМНОЙ темы (скрывается в светлой) */}
+      <Image
+        // Убедись, что у тебя есть версия логотипа со светлым текстом в папке public/logos/
+        src="/logos/menora-logo-dark.svg"
+        alt="Menora Center"
+        width={180}
+        height={60}
+        priority
+        className="h-10 md:h-12 lg:h-14 w-auto object-contain hidden dark:block"
       />
     </Link>
   );
